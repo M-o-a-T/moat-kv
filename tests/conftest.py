f69fbb5b71737f8f6eb14d29cb16ebefa847df7a
Matthias Urlichs <matthias@urlichs.de>
2019-01-03 17:14:29 +0100

    no logging by default

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,7 @@ from pytest_trio.enable_trio_mode import *
 import pytest
 
 import logging
-logging.basicConfig(level=logging.DEBUG)
+#logging.basicConfig(level=logging.DEBUG)
 
 def global_state(pytestconfig):
 	import pdb;pdb.set_trace()
