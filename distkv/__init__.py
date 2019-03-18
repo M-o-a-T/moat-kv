37867eccc5ffc0769ff4ec10d9da1d2616bb3edf
Matthias Urlichs <matthias@urlichs.de>
2019-03-18 12:27:00 +0100

    stupid typo

--- distkv/__init__.py
+++ distkv/__init__.py
@@ -2,7 +2,7 @@
 
 import pkg_resources  # part of setuptools
 _version = pkg_resources.require("distkv")[0].version
-dep pkg_resources
+del pkg_resources
 
 _version_tuple = tuple(int(x) for x in _version.split('.'))
 
