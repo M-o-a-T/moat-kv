2c47224cceb203274e67ed214ab5b68ce66bd305
Matthias Urlichs <matthias@urlichs.de>
2019-03-29 18:49:52 +0100

    work when not installed

--- distkv/__init__.py
+++ distkv/__init__.py
@@ -1,8 +1,12 @@
 """Top-level package for DistKV."""
 
-import pkg_resources  # part of setuptools
-_version = pkg_resources.require("distkv")[0].version
-del pkg_resources
+try:
+	import pkg_resources  # part of setuptools
+	_version = pkg_resources.require("distkv")[0].version
+	del pkg_resources
 
-_version_tuple = tuple(int(x) for x in _version.split('.'))
+	_version_tuple = tuple(int(x) for x in _version.split('.'))
 
+except Exception as exc:
+	_version = "0.0.1"
+	_version_tuple = (0,0,1)
