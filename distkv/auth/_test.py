2f60edd257c08b473a6f28dd4157ab34a832a475
Matthias Urlichs <matthias@urlichs.de>
2019-04-02 14:16:05 +0200

    fix test auth module

--- distkv/auth/_test.py
+++ distkv/auth/_test.py
@@ -36,24 +36,28 @@ class ServerUserMaker(BaseServerUserMaker):
     # Overly-complicated methods of exchanging the user name
 
     @classmethod
-    async def recv(self,cmd,data):
-        cmd.send(step="GiveName")
+    async def recv(cls,cmd,data):
+        await cmd.send(step="GiveName")
         msg = await cmd.recv()
         assert msg.step == "HasName"
+        self = cls()
         self._name = msg.name
+        return self
 
     async def send(self,cmd):
-        cmd.send(step="SendWant")
+        await cmd.send(step="SendWant")
         msg = await cmd.recv()
         assert msg.step == "WantName"
-        cmd.send(step="SendName",name=self._name)
+        await cmd.send(step="SendName",name=self._name)
+        msg = await cmd.recv()
 
-    # Annoying methods to read+save the user name
+    # Annoying methods to read+save the user name from/to KV
 
     @classmethod
     def build(cls,data):
         self=super().build(data)
         self._name = data['UserName']
+        return self
 
     def save(self):
         res = super().save()
@@ -64,6 +68,13 @@ class ServerUser(RootServerUser):
     pass
 
 class ClientUserMaker(BaseClientUserMaker):
+    schema = dict(
+            type="object",
+            additionalProperties=False,
+            properties=dict(
+                name=dict(type="string", minLength=1, pattern="^[a-zA-Z][a-zA-Z0-9_]*$"),
+            ),
+        )
     _name=None
     @property
     def ident(self):
@@ -75,28 +86,32 @@ class ClientUserMaker(BaseClientUserMaker):
     def build(cls,user):
         self=super().build(user)
         self._name=user['name']
+        return self
 
     @classmethod
     async def recv(cls, client: "distkv.client.Client", ident:str, _kind:'user'):
         """Read a record representing a user from the server."""
-        s = await client.stream(action="auth_get",typ=cls._auth_method,kind=_kind,ident=ident)
-        m = await s.recv()
-        assert m.step == "SendWant", m
-        await s.send(step="WantName")
-        m = await s.recv()
-        assert m.name == ident
+        async with client.stream(action="auth_get",typ=cls._auth_method,kind=_kind,ident=ident, stream=True) as s:
+            m = await s.recv()
+            assert m.step == "SendWant", m
+            await s.send(step="WantName")
+            m = await s.recv()
+            assert m.name == ident
 
-        self.= cls()
-        self._name = m.name
-        return self
+            self = cls()
+            self._name = m.name
+            return self
     
-    async def save(self, client: "distkv.client.Client", _kind='user'):
+    async def send(self, client: "distkv.client.Client", _kind='user'):
         """Send a record representing this user to the server."""
-        s = await client.stream(action="auth_set",typ=cls._auth_method,kind=_kind)
-        # we could initially send the ident but don't here, for testing
-        m = await s.recv()
-        assert m.step == "GiveName", m
-        await m.send(step="HasName", name=self._name)
+        async with client.stream(action="auth_set",typ=type(self)._auth_method,kind=_kind, stream=True) as s:
+            # we could initially send the ident but don't here, for testing
+            m = await s.recv()
+            assert m.step == "GiveName", m
+            await s.send(step="HasName", name=self._name)
+            m = await s.recv()
+            assert m.changed
+            assert m.prev is None
 
     def export(self):
         """Return the data required to re-create the user via :meth:`build`."""
