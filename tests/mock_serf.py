b008b97fea9beee365a7ac5bb1d29430273c0eb0
Matthias Urlichs <matthias@urlichs.de>
2019-05-13 19:06:11 +0200

    Test logging: fix time display

--- tests/mock_serf.py
+++ tests/mock_serf.py
@@ -114,6 +114,8 @@ async def stdtest(n=1, run=True, client=True, ssl=False, tocks=20, **kw):
         st = S(tg)
         async with AsyncExitStack() as ex:
             ex.enter_context(mock.patch("time.time", new=tm))
+            logging._startTime = tm()
+
             ex.enter_context(
                 mock.patch("asyncserf.serf_client", new=partial(mock_serf_client, st))
             )
