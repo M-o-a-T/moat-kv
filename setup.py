c9c87855aa63ddb1df095ab0eb59ac8331a05968
Matthias Urlichs <matthias@urlichs.de>
2019-08-31 21:01:20 +0200

    beta, require anyio

--- setup.py
+++ setup.py
@@ -17,6 +17,7 @@ setup(
     install_requires=[
         "asyncclick",
         "trio >= 0.11",
+        "anyio",
         "range_set >= 0.2",
         "attrs >= 18.2",
         "asyncserf >= 0.15.3",
@@ -32,7 +33,7 @@ setup(
     keywords=["async", "key-values", "distributed"],
     python_requires=">=3.7",
     classifiers=[
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 4 - Beta",
         "Intended Audience :: Information Technology",
         "License :: OSI Approved :: MIT License",
         "License :: OSI Approved :: Apache Software License",
