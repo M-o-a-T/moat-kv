df3c6836c760a26bec1364ba10577d89a9bc7a41
Matthias Urlichs <matthias@urlichs.de>
2020-06-25 17:27:29 +0200

    Add distkv.util.spawn
    which returns a cancel scope
    as that's too common a pattern

--- distkv/util.py
+++ distkv/util.py
@@ -1001,3 +1001,31 @@ class ValueEvent:
         """
         await self.event.wait()
         return self.value.unwrap()
+
+
+async def spawn(taskgroup, proc, *args, **kw):
+    """
+    Run a task within this object's task group.
+
+    Returns:
+        a cancel scope you can use to stop the task.
+    """
+
+    scope = None
+    async def _run(proc, args, kw, evt):
+        """
+        Helper for starting a task.
+
+        This accepts a :class:`ValueEvent`, to pass the task's cancel scope
+        back to the caller.
+        """
+        nonlocal scope
+        async with anyio.open_cancel_scope() as sc:
+            scope = sc
+            await evt.set()
+            await proc(*args, **kw)
+
+    evt = anyio.create_event()
+    await taskgroup.spawn(_run, proc, args, kw, evt)
+    await evt.wait()
+    return scope
