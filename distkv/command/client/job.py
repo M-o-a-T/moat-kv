4a5c5ba8147424b72aaaeac1bee044afab4dc033
Matthias Urlichs <matthias@urlichs.de>
2021-02-10 10:10:17 +0100

    rename distkv client run > job

--- distkv/command/client/job.py
+++ distkv/command/client/job.py
@@ -58,12 +58,12 @@ async def path__(obj, path):
     yprint(res, stream=obj.stdout)
 
 
-@cli.command("all")
+@cli.command("run")
 @click.option(
     "-n", "--nodes", type=int, default=0, help="Size of the group (not for single-node runners)"
 )
 @click.pass_obj
-async def all_(obj, nodes):
+async def run(obj, nodes):
     """
     Run code that needs to run.
 
