752c5e69fc2b488b11dc8edc0044f1e25520dc66
Matthias Urlichs <matthias@urlichs.de>
2022-07-21 08:40:55 +0200

    more formatting

--- tests/test_util_times.py
+++ tests/test_util_times.py
@@ -1,29 +1,3 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-from __future__ import absolute_import, print_function, division, unicode_literals
-
-##
-##  This file is part of MoaT, the Master of all Things.
-##
-##  MoaT is Copyright © 2007-2016 by Matthias Urlichs <matthias@urlichs.de>,
-##  it is licensed under the GPLv3. See the file `README.rst` for details,
-##  including optimistic statements by the author.
-##
-##  This program is free software: you can redistribute it and/or modify
-##  it under the terms of the GNU General Public License as published by
-##  the Free Software Foundation, either version 3 of the License, or
-##  (at your option) any later version.
-##
-##  This program is distributed in the hope that it will be useful,
-##  but WITHOUT ANY WARRANTY; without even the implied warranty of
-##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-##  GNU General Public License (included; see the file LICENSE)
-##  for more details.
-##
-##  This header is auto-generated and may self-destruct at any time,
-##  courtesy of "make update". The original is in ‘scripts/_boilerplate.py’.
-##  Thus, do not remove the next line, or insert any blank lines above.
-##BP
 
 # Tests to figure out how long until a time spec fits / does not fit
 
