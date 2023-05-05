c8f3d414be82bfe3a0995cf319499b0fde5f0c3e
Matthias Urlichs <matthias@urlichs.de>
2023-05-05 16:09:54 +0200

    might block due to previous error

--- tests/test_feature_allrunner.py
+++ tests/test_feature_allrunner.py
@@ -42,7 +42,9 @@ async def test_83_run(autojump_clock):  # pylint: disable=unused-argument
                 with trio.fail_after(200):
                     await c._test_evt.wait()
             finally:
-                await st.run("data : get -rd_", do_stdout=False)
+                # this might block due to previous error
+                with anyio.fail_after(2):
+                    await st.run("data : get -rd_", do_stdout=False)
             await trio.sleep(11)
 
             logger.info("End sleep")
