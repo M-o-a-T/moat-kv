8dd9ef649b2ddb3fbbede4596ee13e0e46978625
Matthias Urlichs <matthias@urlichs.de>
2023-05-17 10:56:08 +0200

    Use moat.src.test.run

--- moat/kv/mock/__init__.py
+++ moat/kv/mock/__init__.py
@@ -4,55 +4,23 @@ import logging
 import shlex
 import socket
 import sys
+from pathlib import Path
 
 import attr
 from asyncscope import main_scope, scope
-from moat.util import OptCtx, attrdict, combine_dict, list_ext, load_ext, wrap_main
+from moat.util import OptCtx, attrdict, combine_dict, list_ext, load_ext, wrap_main, yload
+from moat.src.test import run
 
 from moat.kv.client import _scoped_client, client_scope
 
+import logging
+logger = logging.getLogger(__name__)
 try:
     from contextlib import asynccontextmanager
 except ImportError:
     from async_generator import asynccontextmanager
 
-
-async def run(*args, expect_exit=0, do_stdout=True):
-    args = ("-c", "/dev/null", *args)
-    if do_stdout:
-        CFG["_stdout"] = out = io.StringIO()
-    logger.debug(" moat.kv %s", " ".join(shlex.quote(str(x)) for x in args))
-    try:
-        res = None
-        async with OptCtx(
-            main_scope(name="run") if scope.get() is None else None
-        ), scope.using_scope():
-            res = await wrap_main(
-                args=args,
-                wrap=True,
-                CFG=CFG,
-                cfg=False,
-                name="moat.kv",
-                sub_pre="moat.kv.command",
-                sub_post="cli",
-            )
-        if res is None:
-            res = attrdict()
-        return res
-    except SystemExit as exc:
-        res = exc
-        assert exc.code == expect_exit, exc.code
-        return exc
-    except BaseException as exc:
-        res = exc
-        raise
-    else:
-        assert expect_exit == 0
-        return res
-    finally:
-        if do_stdout:
-            res.stdout = out.getvalue()
-            CFG["_stdout"] = sys.stdout
+CFG = yload(Path(__file__).parent.parent / "_config.yaml", attr=True)
 
 
 @attr.s
@@ -116,4 +84,4 @@ class S:
             if isinstance(args, str):
                 args = args.split(" ")
         async with scope.using_scope():
-            return await run("-v", "-v", "client", "-h", h, "-p", p, *args, do_stdout=do_stdout)
+            return await run("-v", "-v", "kv", "-h", h, "-p", p, *args, do_stdout=do_stdout)
