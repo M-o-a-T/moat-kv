c82747a756f16bd43d6d8526e73118896c51e90d
Matthias Urlichs <matthias@urlichs.de>
2024-03-05 17:24:31 +0100

    suppress stupid warnings

--- moat/kv/__init__.py
+++ moat/kv/__init__.py
@@ -3,10 +3,13 @@
 __path__ = __import__("pkgutil").extend_path(__path__, __name__)
 
 try:
+    import warning
     import pkg_resources  # part of setuptools
 
-    _version = pkg_resources.require("moat.kv")[0].version
+    with warnings.filterwarnings("ignore"):
+        _version = pkg_resources.require("moat.kv")[0].version
     del pkg_resources
+    del warnings
 
     _version_tuple = tuple(int(x) for x in _version.split("."))
 
